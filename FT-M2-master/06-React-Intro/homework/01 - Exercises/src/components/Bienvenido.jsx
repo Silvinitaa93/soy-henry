62fd5d2322e58fc9d39d18e4879e2d8f2aa2ecee
Silvinitaa93 <silvinadnavarro@gmail.com>
2023-03-09 22:36:38 -0300

    hecho

--- FT-M2-master/06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ FT-M2-master/06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import Botones from "./Botones";
 
-const studentName = "";
+const studentName = "Silvina Navarro";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
+  return (
+    <div>
+      <h1>Hola, no te rindas!</h1>
+      <h3>{studentName}</h3>
+      <ul>
+        {techSkills.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+      <Botones alerts={alerts} />
+    </div>
+  );
 }
 
 // Esto lo exportamos para los tests
